1732fe7189c4e3ce015e2bc90854c0f9d6bd291c
Roma ZpUa <roma.zp.ua@gmail.com>
2023-05-13 12:24:04 +0200

    add tags receiving

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid'
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
 
@@ -15,9 +15,11 @@ export const Home = () => {
     const { posts, tags } = useSelector( state => state.posts )
 
     const isPostLoading = posts.status === 'loading'
+    const isTagsLoading = tags.status === 'loading'
 
     useEffect( () => {
         dispatch( fetchPosts() )
+        dispatch( fetchTags() )
     }, [] )
 
     return (
@@ -32,24 +34,20 @@ export const Home = () => {
                         isPostLoading ?
                             ( <Post key={ index } isLoading={ true }/> ) :
                             ( <Post
-                                id={ 1 }
-                                title="Roast the code #1 | Rock Paper Scissors"
+                                id={ obj._id }
+                                title={ obj.title }
                                 imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-                                user={ {
-                                    avatarUrl:
-                                        'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                                    fullName: 'Keff',
-                                } }
-                                createdAt={ '12 июня 2022 г.' }
-                                viewsCount={ 150 }
+                                user={ obj.user }
+                                createdAt={ obj.createdAt }
+                                viewsCount={ obj.viewsCount }
                                 commentsCount={ 3 }
-                                tags={ [ 'react', 'fun', 'typescript' ] }
+                                tags={ obj.tags }
                                 isEditable
                             /> )
                     ) }
                 </Grid>
                 <Grid xs={ 4 } item>
-                    <TagsBlock items={ [ 'react', 'typescript', 'заметки' ] } isLoading={ false }/>
+                    <TagsBlock items={ tags.items } isLoading={ isTagsLoading }/>
                     <CommentsBlock
                         items={ [
                             {
